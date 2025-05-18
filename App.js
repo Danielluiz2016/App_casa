7dfd0fceb4e56ac9ba9d776025c7ad1c71dbf6f0
Daniel luiz <danielluiz691@gmail.com>
2025-05-18 13:18:13 -0300

    sdsdsdsds

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView source={{ uri: 'https://controle.squareweb.app' }} style={styles.webview} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
